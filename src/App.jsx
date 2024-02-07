034f155379f606a7c91cfc0c5722261260a5a416
akshay <akshaykatoch38@gmail.com>
2024-02-07 23:01:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,145 @@
+import axios from 'axios';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 40;
+
+function App() {
+  const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${searchInput.current.value}&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`
+        );
+        setImages((prevImages) => [...prevImages, ...data.results]);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
+    } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
+      console.log(error);
+      setLoading(false);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    setImages([]);
+    fetchImages();
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    resetSearch();
+  };
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+    resetSearch();
+  };
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const downloadImage = (imageUrl) => {
+    const link = document.createElement('a');
+    link.href = imageUrl;
+    link.download = 'image.jpg'; // You can customize the downloaded file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className='container'>
+      <h1 className='title'>PicFinder</h1>
+      {errorMsg && <p className='error-msg'>{errorMsg}</p>}
+      <div className='search-section'>
+        <Form onSubmit={handleSearch}>
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+      <div className='filters'>
+        <div onClick={() => handleSelection('cars')}>Cars</div>
+        <div onClick={() => handleSelection('bikes')}>Bikes</div>
+        <div onClick={() => handleSelection('watches')}>Watches</div>
+        <div onClick={() => handleSelection('shoes')}>Shoes</div>
+      </div>
+      {loading ? (
+        <p className='loading'>Loading...</p>
+      ) : (
+        <>
+          <div className='images'>
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className='image'
+                onClick={() => openModal(image)}
+              />
+            ))}
+          </div>
+          <div className='buttons'>
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Load More</Button>
+            )}
+          </div>
+        </>
+      )}
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Image Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedImage && (
+            <div>
+              <img
+                src={selectedImage.urls.regular}
+                alt={selectedImage.alt_description}
+                style={{ width: '100%', marginBottom: '10px' }}
+              />
+              <p>Author: {selectedImage.user.username}</p>
+              <p>Description: {selectedImage.description || 'N/A'}</p>
+              <Button variant='primary' onClick={() => downloadImage(selectedImage.urls.full)}>Download Image</Button>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={closeModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
